50aaf3484cd328cde4b8c74d52034510eb8420f4
HcySunYang <HcySunYang@outlook.com>
2019-10-27 18:46:16 +0800

    feat: export useTimeout

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as useToggle } from './useToggle'
 export { default as useBoolean } from './useBoolean'
 export { default as useCounter } from './useCounter'
+export { default as useTimeout } from './useTimeout'
