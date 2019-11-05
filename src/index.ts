728a4ef5336ab6cdf07f4b8d11b9206cc3840e24
Ruijia Tang <413732712@qq.com>
2019-11-05 16:23:46 +0800

    feat(useLocalStorage): add story (#4)

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ export { default as useGeolocation } from './useGeolocation'
 export { default as useMouse } from './useMouse'
 export { default as useOnline } from './useOnline'
 export { default as useVisibilityState } from './useVisibilityState'
+export { default as useLocalStorage } from './useLocalStorage'
 
 // utils
 import throttle from 'lodash.throttle'
